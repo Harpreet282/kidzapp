f76fc5f33e85062adb01e47cc291733c8a2f45c2
priyabangarh84@gmail.com <priyabangarh84@gmail.com>
2022-06-20 12:34:55 +0530

    api commit

--- src/APIs/apis.js
+++ src/APIs/apis.js
@@ -2,9 +2,6 @@ export const API_BASE_URL = "https://api2.kidzapp.com/api/3.0";
 
 export  const getApiUrl = (endpoint) => API_BASE_URL + endpoint;
 
-
-
-
 export const BANNER_DATA_API = getApiUrl('/experiences/curated-list/?list_name=featured_banner_uae&country_code=&page=1&page_size=10&city=&website=1');
 export const COLLECTION_API = getApiUrl('/lists?country_code=ae');
 export const KIDZAPPOLIS_API = getApiUrl('/categories?country_code=ae');
